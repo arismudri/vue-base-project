51065f4216b8e0b6f57c916f9bd7232f3f777816
aris16 <aris@bts.id>
2022-03-05 21:37:10 +0700

    refactor folder structure

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@/modules/app/views/App.vue'
+import router from '@/modules/app/router'
 
 Vue.config.productionTip = false
 
---
+++ src/modules/app/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const importRoutesModules = () => {
+  const context = require.context('@/modules', true, /router.js$/i)
+  return context.keys().map(context).map(m => m.default)
+}
+
+const routes = [
+  {
+    path: '/',
+    component: () => import(/* webpackChunkName: "about" */ '@/modules/app/views/components/AppRouteEntryPoint.vue')
+  },
+  ...importRoutesModules()
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
