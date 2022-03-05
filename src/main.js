bdfcc38f1b19c6a1eb2c4a9ce58751321aa806e9
aris16 <aris@bts.id>
2022-03-05 21:56:53 +0700

    add vuetify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from '@/modules/app/views/App.vue'
 import router from '@/modules/app/router'
+import vuetify from '@/plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
